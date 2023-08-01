bac5deb1658bec4e9bb60ac6b3a3200531a96e88
Maria Soria <maria.it3@gmail.com>
2023-08-01 19:50:36 +0100

    Remove generation of uuid id and set name as unique constraint for mongodb. Clean application.properties

--- src/main/kotlin/com/codingiscaring/envmanagerbackend/repositories/entities/EnvironmentEntity.kt
+++ src/main/kotlin/com/codingiscaring/envmanagerbackend/repositories/entities/EnvironmentEntity.kt
@@ -1,21 +1,19 @@
 package com.codingiscaring.envmanagerbackend.repositories.entities
 
 import com.codingiscaring.envmanagerbackend.models.Environment
-import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
-import java.util.UUID
 
 @Document(collection = "environments")
 class EnvironmentEntity {
-    @Id
-    lateinit var id: UUID
+
+    @Indexed(unique = true)
     lateinit var name: String
     var description: String? = null
 
     companion object {
         fun mapFrom(environment: Environment): EnvironmentEntity {
             return EnvironmentEntity().apply {
-                id = UUID.randomUUID()
                 name = environment.name
                 description = environment.description
             }
