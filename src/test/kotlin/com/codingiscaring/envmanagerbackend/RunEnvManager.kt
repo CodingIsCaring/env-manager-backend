216fdd2a025544924dc8e03ce5b8ab99461cd93b
Aleix Morgadas <hello@aleixmorgadas.dev>
2023-09-06 18:20:41 +0200

    good configuration

--- src/test/kotlin/com/codingiscaring/envmanagerbackend/RunEnvManager.kt
+++ src/test/kotlin/com/codingiscaring/envmanagerbackend/RunEnvManager.kt
@@ -1,5 +1,6 @@
 package com.codingiscaring.envmanagerbackend
 
+import com.github.dockerjava.api.command.InspectContainerResponse
 import org.springframework.boot.devtools.restart.RestartScope
 import org.springframework.boot.fromApplication
 import org.springframework.boot.test.context.TestConfiguration
@@ -8,6 +9,7 @@ import org.springframework.boot.with
 import org.springframework.context.annotation.Bean
 import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.MongoDBContainer
+import org.testcontainers.containers.wait.strategy.Wait
 import org.testcontainers.utility.DockerImageName
 
 fun main(args: Array<String>) {
@@ -22,15 +24,17 @@ class Configuration {
     @Bean
     @RestartScope
     @ServiceConnection
-    fun mongoContainer(): MongoDBContainer {
-        val container = MongoDBContainer(DockerImageName.parse("mongo:latest"))
-            .withLabel(
-                "com.testcontainers.desktop.service",
-                "envmanager"
-            )
-            .withSharding() // Fixes boot issue when using default configuration (replica-set) with volume mounting
-            .withFileSystemBind("./.data", "/data/db")
-        container.portBindings = listOf("27017:27017")
-        return container
+    fun mongoContainer() = StatefulMongoDBContainer(DockerImageName.parse("mongo:6.0.8"))
+
+    class StatefulMongoDBContainer(imageName: DockerImageName) : MongoDBContainer(imageName) {
+        override fun configure() {
+            waitingFor(Wait.forLogMessage("(?i).*waiting for connections.*", 1));
+            withFileSystemBind("./.data", "/data/db", BindMode.READ_WRITE)
+            portBindings = listOf("27019:27017")
+        }
+
+        override fun containerIsStarted(containerInfo: InspectContainerResponse?, reused: Boolean) {
+            // do nothing
+        }
     }
 }
